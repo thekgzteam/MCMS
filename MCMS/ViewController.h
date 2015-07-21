fd253a010587b181ea321c8d3d6e2c4a641bab0f
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-07-21 14:58:04 -0500

    Initial Commit

---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Edil Ashimov on 7/21/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
